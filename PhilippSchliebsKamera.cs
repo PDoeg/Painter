267ef9bc7a0c015d83f59ef19d397dd983b8c0a3
Gamer <Gamer>
2017-03-28 10:57:07 +0200

    u know what it is

---
+++ PhilippSchliebsKamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class kamera : MonoBehaviour {
+
+	public int maxScore = 20;
+	public static float memory = 0;
+	private VignetteAndChromaticAberration script;
+
+	// Use this for initialization
+	void Start () {
+		script = GetComponent <VignetteAndChromaticAberration> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		script.chromaticAberration = Mathf.Lerp (0, 100, (float)Score.score / (float)maxScore);
+		memory = Mathf.Lerp (0, 100, (float)Score.score / (float)maxScore);
+	}
+	
+	void DoSomething()
+	{
+		Debug.Log("i am doing something");
+	}
+}
